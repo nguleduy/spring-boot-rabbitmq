f3950294969220217a42f12a378d91eeb4c8469a
Joseph <nguld@itsj-group.com>
2020-12-22 16:26:38 +0700

    init commit

---
+++ rabbitmq/src/main/java/com/example/rabbitmq/consumer/ListenerConsumer.java
@@ -0,0 +1,15 @@
+package com.example.rabbitmq.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListenerConsumer {
+
+  //It expects QueueObject but we published String so we got error. To handle it, we've created generic object as
+  // expected parameter.
+  @RabbitListener(queues = {"${rabbitmq.direct.queue.1}","${rabbitmq.direct.queue.2}"}, containerFactory = "createListener")
+  public void receiveMessages(Object object) {
+    System.out.println(object);
+  }
+}
---
+++ rabbitmq/src/main/java/com/example/rabbitmq/controller/DefaultController.java
@@ -0,0 +1,22 @@
+package com.example.rabbitmq.controller;
+
+import com.example.rabbitmq.model.QueueObject;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class DefaultController {
+
+  @Autowired
+  private AmqpTemplate defaultQueue;
+
+  @GetMapping("/default")
+  public void sendDefault() {
+    QueueObject object = new QueueObject("default", LocalDateTime.now());
+    defaultQueue.convertAndSend(object);
+  }
+}
